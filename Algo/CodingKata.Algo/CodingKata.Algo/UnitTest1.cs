59685cbc13d2a42084ade8a4c7531dbe5dd1863e
Théo <fraternity.theo@gmail.com>
2022-06-29 16:34:29 +0200

    adding algo exercise

---
+++ Algo/CodingKata.Algo/CodingKata.Algo/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace CodingKata.Algo
+{
+    public class Tests
+    {
+        public int[] SortSocks(int[] arr)
+        {
+            return arr;
+        }
+
+        [Test]
+        [TestCase(new int[0] { }, 0)]
+        [TestCase(new int[4] { 1, 1, 2, 2 }, 0)]
+        [TestCase(new int[4] { 1, 1, 2, 2 }, 0)]
+        [TestCase(new int[4] { 1, 1, 1, 2 }, 2)]
+        [TestCase(new int[4] { 1, 1, 1, 1 }, 0)]
+        [TestCase(new int[5] { 1, 1, 2, 2, 3 }, 1)]
+        [TestCase(new int[5] { 1, 1, 1, 1, 0 }, 1)]
+        [TestCase(new int[5] { 1, 4, 2, 1, 0 }, 3)]
+        public void Test1(int[] arr, int expected)
+        {
+            var result = SortSocks(arr);
+
+            Assert.That(result.Length == expected);
+            Assert.That(result.Distinct().Count() == result.Length);
+        }
+    }
+}
